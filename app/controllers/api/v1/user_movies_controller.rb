d8f62bd05e8834c9b65bac654af2b533b5f5c608
juannodeb <juannodeb@gmail.com>
2021-01-16 19:31:36 -0500

    feat: Added controller folders/files and updated the routing

---
+++ app/controllers/api/v1/user_movies_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class Api::V1::UserMoviesController < ApplicationController
+  def index
+    user_movies = UserMovie.where(user_id: params[:user_id])
+    movies = Movie.where(id: user_movies.map(&:id))
+
+    res = movies.empty? ? { message: 'You have no movies in your favorites list' } : movies
+
+    render json: res, except: [:created_at, :updated_at]
+  end
+end
