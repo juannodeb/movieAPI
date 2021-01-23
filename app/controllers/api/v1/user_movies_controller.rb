a112fd07c282fe36304b684be75c890823b5bb7d
juannodeb <juannodeb@gmail.com>
2021-01-22 21:08:50 -0500

    feat: Added create method for the user_movies controller and updated the index method, added the controller to the route. Added Record Invalid custom message

--- app/controllers/api/v1/user_movies_controller.rb
+++ app/controllers/api/v1/user_movies_controller.rb
@@ -3,12 +3,50 @@
 class Api::V1::UserMoviesController < ApplicationController
   before_action :authenticate_api_v1_user!
 
+  # Renders the current user favorite movies or message in case they have no favorite movies yet
   def index
     user_movies = UserMovie.where(user_id: params[:user_id])
     movies = Movie.where(id: user_movies.map(&:id))
 
-    res = movies.empty? ? { message: 'You have no movies in your favorites list' } : movies
+    if movies.empty?
+      message = 'You have no movies in your favorites list'
+    else
+      message = 'Favorite list fetched successfully'
+      user_movies = movies
+    end
 
-    render json: res, except: [:created_at, :updated_at]
+    render json: {
+      success: true,
+      message: message,
+      user_movies: user_movies
+    }, status: status, except: [:created_at, :updated_at]
+  end
+
+  # Creates an UserMovie record with given ids or returns error message when missing
+  def create
+    user_movie = UserMovie.new(user_movie_params)
+
+    if user_movie.save!
+      success = true
+      message = 'UserMovie created successfully'
+      status = :ok
+    else
+      success = false
+      message = 'Could not create the UserMovie'
+      status = :internal_server_error
+    end
+
+    render json: {
+      success: success,
+      message: message,
+      user_movie: user_movie
+    }, status: status, except: [:created_at, :updated_at]
+  end
+
+  private
+
+  # Needed fields to create a UserMovie record (an user favorite movie)
+  def user_movie_params
+    params.require(:user_movie).permit(:user_id, :movie_id)
   end
 end
