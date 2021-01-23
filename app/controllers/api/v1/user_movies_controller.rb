14b96256650a07bc00ff791a325a6768307e2f5d
juannodeb <juannodeb@gmail.com>
2021-01-23 13:33:16 -0500

    fix: Added inline doc for controllers, updated a couple error messages and the create method for user_movies controller

--- app/controllers/api/v1/user_movies_controller.rb
+++ app/controllers/api/v1/user_movies_controller.rb
@@ -3,7 +3,34 @@
 class Api::V1::UserMoviesController < ApplicationController
   before_action :authenticate_api_v1_user!
 
-  # Renders the current user favorite movies or message in case they have no favorite movies yet
+  # ===== Name:
+  # Returns the existing user_movies for the current_user
+  # ===== URL:
+  # /api/v1/favorites
+  # ===== Action:
+  # GET
+  # ===== Return:
+  # {
+  #  "success": true,
+  #  "message": "Favorites list fetched successfully",
+  #  "user_movies": [
+  #   {
+  #    "id": 1,
+  #    "title": "My movie",
+  #    "director": "John Doe",
+  #    "year": "2001",
+  #    "image": "my_poster.jpg",
+  #    "description": "My description"
+  #   },
+  #   ...
+  #  ]
+  # }
+  # or if the user has no user_movies:
+  # {
+  #   "success": true,
+  #   "message": "You have no movies in your favorites list",
+  #   "user_movies": []
+  # }
   def index
     user_movies = UserMovie.where(user_id: params[:user_id])
     movies = Movie.where(id: user_movies.map(&:movie_id))
@@ -22,7 +49,26 @@ class Api::V1::UserMoviesController < ApplicationController
     }, status: status, except: [:created_at, :updated_at]
   end
 
-  # Renders an specific favorites list movie record
+  # ===== Name:
+  # Returns an specific favorites list movie
+  # ===== URL:
+  # /api/v1/favorites/:id
+  # ===== Action:
+  # GET
+  # ===== Parameters:
+  # * +id+ : Integer
+  # ===== Return:
+  # {
+  #   "success": true,
+  #   "message": "Favorites list movie fetch successfully",
+  #   "user_movie": [
+  #       {
+  #           "id": 10,
+  #           "user_id": 1,
+  #           "movie_id": 2
+  #       }
+  #   ]
+  # }
   def show
     user_movie = UserMovie.where(movie_id: params[:user_movie][:movie_id])
 
@@ -43,28 +89,65 @@ class Api::V1::UserMoviesController < ApplicationController
     }, status: status, except: [:created_at, :updated_at]
   end
 
-  # Creates an UserMovie record with given ids or returns error message when missing
+  # ===== Name:
+  # Creates an UserMovie record with the given ids or returns error message when missing
+  # ===== URL:
+  # /api/v1/favorites/:id
+  # ===== Action:
+  # POST
+  # ===== Parameters:
+  # * +user_id+ : Integer
+  # * +movie_id+ : Integer
+  # ===== Return:
+  # {
+  #  "success": true,
+  #  "message": "UserMovie updated or created successfully",
+  #  "user_movie": {
+  #   "id": 1,
+  #   "user_id": 1,
+  #   "movie_id": 1
+  #  }
+  # }
+  # or if the user_id/movie_id are missing:
+  # {
+  #  "success": false,
+  #  "message": "Could not create the record. User or Movie ids are missing"
+  # }
   def create
-    user_movie = UserMovie.find_or_create_by(user_movie_params)
+    already_created = UserMovie.where(user_movie_params).present?
+    user_movie = UserMovie.new(user_movie_params)
 
-    if user_movie.save!
+    if !already_created && user_movie.save!
       success = true
       message = 'UserMovie updated or created successfully'
       status = :ok
     else
       success = false
-      message = 'Could not create the UserMovie'
+      message = 'Could not create the UserMovie or it already exists'
       status = :internal_server_error
+      user_movie = []
     end
 
-    render json: {
-      success: success,
-      message: message,
-      user_movie: user_movie
-    }, status: status, except: [:created_at, :updated_at]
+    render json: { success: success, message: message,
+                   user_movie: user_movie }, status: status, except: [:created_at, :updated_at]
   end
 
-  # Removes given UserMovie id record when the user doesn't want the movie on their favorites list
+  # ===== Name:
+  # Removes an UserMovie record with the given id or returns error message when already removed
+  # ===== URL:
+  # /api/v1/favorites/:id
+  # ===== Action:
+  # DELETE
+  # ===== Parameters:
+  # * +id+ : Integer
+  # ===== Return:
+  # if success:
+  #
+  # if it was already removed:
+  # {
+  #  "success": false,
+  #  "message": "Could not remove the UserMovie or it was already removed"
+  # }
   def destroy
     user_movie = UserMovie.where(
       movie_id: params[:user_movie][:movie_id],
